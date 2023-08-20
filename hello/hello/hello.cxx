6a9f1680571a5b2ef57131cacdfbd9817166e322
William Roy <wroy@proton.me>
2023-08-20 19:24:12 -0400

    feat: bootstrap windows app runtime framework package

--- hello/hello/hello.cxx
+++ hello/hello/hello.cxx
@@ -1,13 +1,35 @@
+#include <Windows.h>
+
+#ifdef GetCurrentTime
+#undef GetCurrentTime
+#endif
+
+#include <winrt/Microsoft.UI.Xaml.h>
+#include <MddBootstrap.h>
+#include <WindowsAppSDK-VersionInfo.h>
+
+using namespace winrt;
+using namespace winrt::Microsoft;
+using namespace winrt::Microsoft::UI;
+using namespace winrt::Microsoft::UI::Xaml;
+using namespace ::Microsoft::WindowsAppSDK;
+
 int
-main (int argc, char* argv[])
+main (int argc, char* argv [])
 {
-  using namespace std;
+  init_apartment (apartment_type::single_threaded);
 
-  if (argc < 2)
+  MddBootstrapInitialize2 (Release::MajorMinor,
+                           Release::VersionTag,
+                           PACKAGE_VERSION {Runtime::Version::UInt64},
+                           MddBootstrapInitializeOptions_OnNoMatch_ShowUI);
+
+  Application::Start ([] (auto&&)
   {
-    cerr << "error: missing name" << endl;
-    return 1;
-  }
+    Application a;
+    Window w;
+    w.Activate ();
+  });
 
-  cout << "Hello, " << argv[1] << '!' << endl;
+  MddBootstrapShutdown ();
 }
